cab9e76f4fa6e20645d56587e19784dd3c54c706
George Hamilton <george.hamilton@ed.ac.uk>
2014-02-03 16:55:13 +0000

    add bare bones map templates

---
+++ js/offline-maps.js
@@ -0,0 +1,16 @@
+define(['ui', 'map'], function(ui, map){
+    console.log(map);
+
+    //ui.toggleActiveAdd('download', 'download-page');
+
+    //$(document).on('pageshow', '#saved-maps=page',  offlineMaps);
+    //$(document).on('pageshow', '#save-map', saveMap);
+
+    var offlineMaps = function(){
+        console.log("jings");
+    };
+
+    var saveMap = function(){
+        console.log("crivvens");
+    };
+});
