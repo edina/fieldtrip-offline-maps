b8e8fbab09fd9f9d1bb0dad1971bc4a965e18470
Ruben Gamez <ruben.gamez@ed.ac.uk>
2014-10-17 17:00:22 +0100

    Add comments to the tests

--- src/www/js/tests.js
+++ src/www/js/tests.js
@@ -43,16 +43,22 @@ unit: {
         module("Offline Maps");
 
         if(utils.isMobileDevice() || utils.isChrome()){
+
+            // Test map deletion
             asyncTest("Delete Map", function(){
                 var MAP_NAME = "name-name";
                 expect( 2 );
                 var success = true;
+
+                // Delete a map
                 cache.deleteSavedMapDetails(MAP_NAME);
                 //stop();
                 setTimeout(function(){
                     ok(success, "Database Map Deleted");
                     //check local storage
                     var maps = cache.getSavedMaps() || {};
+
+                    // Assert that map was deleted
                     ok(!(maps[MAP_NAME]), "Map should not be in localStorage" );
                     start();
                 }, 2000);
@@ -60,6 +66,7 @@ unit: {
         }
 
         if(utils.isMobileDevice()){
+            // Test map downloading
             test("Download North edinburgh test", function() {
                 var MAP_NAME = "name-name";
                 expect(3);
@@ -70,12 +77,15 @@ unit: {
                     677925.01823658
                 );
 
-                //first map is container
+                // Zoom into north Edinburgh
                 map.zoomToExtent(bounds);
 
+                // Save the map in the cache
                 cache.saveMap(MAP_NAME, 8, 9);
 
                 stop();
+
+                //Wait two seconds
                 setTimeout(function(){
                     var url ='';
                     var tileExists = false;
@@ -106,12 +116,16 @@ unit: {
                         checkIfFileExists(fileLocation);
                     };
 
+                    // Get the path for a tile inside of the bounds
                     webdb.getCachedTilePath(callback, null, 316,661, 8, 'dummyUrl');
 
                     setTimeout( function() {
                         var maps = ui.cache.getSavedMaps();
+                        // Assert that the map was created
                         ok((maps[MAP_NAME]), "Map should  be in localStorage" );
+                        // Assert that the tile has the right path
                         ok(url.indexOf('edina/cache/name-name/1/open_8_316_661.jpg') !== -1, 'Tile Stored in correct directory');
+                        // Assert that the tile exists
                         ok(tileExists === true, "Actual tile stored in phone directory");
 
                         start();
@@ -123,11 +137,30 @@ unit: {
 },
 sys:{
     run: function() {
+
+        /*
+            Feature: Offline Maps
+                In order to use the maps without connection
+                as a user
+                I want to select and area in the map and download it
+                And visualized it
+                And if I don't need it anymore delete it
+            Scenario: Download Map
+                Given that the user is in the save-map page
+                And the user clicks the download map button
+                Then the user select a zoom level using a slider
+                When the user clicks the save button
+                Then a popup is displaying requesting the name of the map
+                Then the user can customize the name
+                When the user clicks the save button
+                Then the map tiles are saved in the application cache
+        */
         module("Offline Maps");
         if(utils.isMobileDevice() || utils.isChrome()){
             asyncTest("Save Map", function(){
                 var savedMaps = cache.getSavedMaps();
 
+                // Make space for one map
                 if(savedMaps !== undefined){
                     if(cache.getSavedMapsCount() === 3){
                         // delete one of the maps
@@ -140,34 +173,41 @@ sys:{
                 }
                 var mapCount = cache.getSavedMapsCount();
 
+                // Change to the save-map page
                 sts.changePageByFile('save-map.html', '#save-map-page', function(){
                     ok(true, 'navigate to save map page');
 
                     sts.tapAndTest({
                         'id': '#save-map-buttons-ok',
                         'test': function(){
+                            // Assert that the zoom control is visible
                             return $('#cache-controls').is(':visible');
                         },
                         'cb': function(success){
                             ok(success, 'Save button');
+
+                            // Click the save button
                             sts.clickAndTest({
                                 'id': '#cache-save-details-button-div > a',
                                 'delay': 1000,
                                 'test': function(){
+                                    // Assert that the dialog for customizing the map name is visible
                                     return $('#save-map-name-dialog').is(':visible');
                                 },
                                 'cb': function(success){
                                     ok(success, 'Map name dialog');
 
+                                    // Customize the map name
                                     var mapName = 'test_' + $('#saved-map-name-dialog-text').val();
                                     $('#saved-map-name-dialog-text').val(mapName);
 
+                                    // Tap the save button
                                     sts.tapAndTest({
                                         'id': '#saved-map-name-dialog-btn',
                                         'delay': 1000,
                                         'poll': 1000,
                                         'test': function(){
-                                            //Check that the map was saved
+                                            // Assert that the map was saved
                                             for(var key in cache.getSavedMaps()){
                                                 if(key === utils.santiseForFilename(mapName)){
                                                     return true;
